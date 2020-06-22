3a6455eb5f0c7bc6077978695f61873367fdeb2f
Saurav Prakash <33094719+spdev7@users.noreply.github.com>
2020-06-23 00:13:46 +0530

    fd

--- src/App.js
+++ src/App.js
@@ -3,12 +3,32 @@ import ButtonGrid from "./components/ButtonGrid";
 import {FaShoppingCart} from 'react-icons/fa'
 
 const Navbar = (props) => {
+
+  const [search,Setsearch] = useState('');
+
+  const handleChange = (event) => {
+    //[event.target.name] = event.target.value;
+    //console.log(search)
+    Setsearch(event.target.value)
+    const array1 = [{a: 1}, {b: 2}, {c :3}]
+    
+    function isCherries(fruit){
+      return 
+    }
+    // for (let [key, value] of Object.entries(array1)) {
+    //   if(`${key}` == search){
+    //     console.log(`${value}`)
+    //   }
+
+    // }
+  }
   return (
     <div>
       <nav className="navbar navbar-light bg-light justify-content-start mb-2">
         <span className="navbar-brand h1"><FaShoppingCart/></span>
         <span className="badge badge-pill badge-info">{props.noi}</span>
         Items
+        <input type="text" placeholder="enter the item" onChange={handleChange} name="search" />
       </nav>
     </div>
   );
